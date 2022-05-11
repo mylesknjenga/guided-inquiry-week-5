04730e1cf7f26b81a80cdeee30ad518fea1f54fc
Myles Njenga <Myles@MacBook-Pro-4.attlocal.net>
2022-05-11 19:17:52 -0400

    added hard activity

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ const findTarget = (arr, target) => {
 //TESTS
 findTarget([4,5,6,7,0,1,2], 0);
 findTarget([4,5,6,7,0,1,2], 3);
+
+
+
+//HARD - THIS ACTIVITY CAN BE FOUND IN THE INDEX.HTML FILE
+
+// // #DOM Manipulation - Easy
+// // Goal: In JavaScript - Create two buttons, one that when clicked turns the background 
+// of the body red. The other, when clicked, turns the body's background color white. Each of 
+// the background colors should be defined as class styles and when the buttons are pushed the 
+// body's classList is updated to include the correct class for the background
+
+// // Create a single function that takes an input className and updates the body's classList 
+// to ONLY include that className
